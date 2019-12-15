1606bda63453426f44e1b5030f3947c66ca120ce
metal-chaos <nightrage.metal66@gmail.com>
2019-12-15 15:52:24 +0900

    added functions login and logout

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,3 +4,9 @@ use Illuminate\Http\Request;
 
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
