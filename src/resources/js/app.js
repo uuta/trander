4444384edf5182b6edca9d1ae970a9ca3f002920
metal-chaos <nightrage.metal66@gmail.com>
2019-12-17 13:08:16 +0900

    added files for auth store

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
 import './bootstrap'
+import store from './store'
 
 new Vue({
     el: '#app',
     router, // ルーティングの定義を読み込む
+    store,
     components: {
         App
     }, // ルートコンポーネントの使用を宣言する
