4b34893886c0df05aab8817ae3bc3c7e369dab06
metal-chaos <nightrage.metal66@gmail.com>
2019-12-17 14:10:31 +0900

    added UI for login and register

---
+++ src/resources/js/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="navbar">
+    <RouterLink class="navbar__brand" to="/">
+      Trander
+    </RouterLink>
+    <div class="navbar__menu">
+      <span class="navbar__item">
+        username
+      </span>
+      <div class="navbar__item">
+        <RouterLink class="button button--link" to="/login">
+          Login / Register
+        </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
--- src/resources/js/pages/Login.vue
+++ src/resources/js/pages/Login.vue
@@ -1,3 +1,67 @@
 <template>
-    <h1>Login Page</h1>
+  <div class="container--small">
+    <ul class="tab">
+      <li
+        class="tab__item"
+        :class="{'tab__item--active': tab === 1}"
+        @click="tab = 1"
+      >ログイン</li>
+      <li
+        class="tab__item"
+        :class="{'tab__item--active': tab === 2}"
+        @click="tab = 2"
+      >会員登録</li>
+    </ul>
+    <div class="panel" v-show="tab === 1">
+      <form class="form" @submit.prevent="login">
+        <label for="login-email">メールアドレス</label>
+        <input type="email" class="form__item" id="login-email" v-model="loginForm.email" placeholder="メールアドレスを入力してください">
+        <label for="login-password">パスワード</label>
+        <input type="password" class="form__item" id="login-password" v-model="loginForm.password"  placeholder="パスワードを入力してください">
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">ログインする</button>
+        </div>
+      </form>
+    </div>
+    <div class="panel" v-show="tab === 2">
+      <form class="form" @submit.prevent="register">
+        <label for="login-email">お名前</label>
+        <input type="text" class="form__item" id="login-name" v-model="registerForm.name" placeholder="お名前を入力してください">
+        <label for="login-email">メールアドレス</label>
+        <input type="email" class="form__item" id="login-email" v-model="registerForm.email" placeholder="メールアドレスを入力してください">
+        <label for="login-password">パスワード</label>
+        <input type="password" class="form__item" id="login-password" v-model="registerForm.password"  placeholder="パスワードを入力してください">
+        <div class="form__button">
+          <button type="submit" class="button button--inverse">会員登録する</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 1,
+      loginForm: {
+        email: '',
+        password: ''
+      },
+      registerForm: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.loginForm)
+    },
+    register () {
+      console.log(this.registerForm)
+    },
+  }
+}
+</script>
