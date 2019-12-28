626ff6f94ef92bd49867b33fe6deeeb83037b1c1
metal-chaos <nightrage.metal66@gmail.com>
2019-12-28 13:40:30 +0900

    modified setting in vueGoogleMaps.js

--- src/resources/js/vueGoogleMaps.js
+++ src/resources/js/vueGoogleMaps.js
@@ -1,8 +1,9 @@
-import * as VueGoogleMaps from '~/node_modules/vue2-google-maps'
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: env('API_FOR_GOOGLE_MAPS'),
+    key: process.env.MIX_API_FOR_GOOGLE_MAPS,
     libraries: 'places'
   }
 })
