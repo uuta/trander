3354d192c116aea2c85a87107a7b04c3c9f53f11
metal-chaos <nightrage.metal66@gmail.com>
2020-01-06 15:06:32 +0900

    added controller to get api response from RESAS

--- src/routes/api.php
+++ src/routes/api.php
@@ -24,20 +24,4 @@ Route::post('/regenerate-password', 'Auth\ForgotPasswordController@callResetPass
 Route::get('/social/{social}', 'Auth\LoginController@socialLogin')->name('social-login');
 Route::get('/social/callback/{social}', 'Auth\LoginController@socialCallback')->name('social-callback');
 
-// Guzzleモジュールのクラス読み込み
-use GuzzleHttp\Client;
-
-Route::get("/data", function () {
-    $client = new Client();
-    $sourceUrl = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
-    $responseData = $client->request("GET", $sourceUrl, array(
-        "headers" => array(
-            "X-API-KEY" => 'sOVOWs4CXZbBiqrR1lu88W2SsxKgWqg4ssir5M8u',
-        ),
-    ));
-    $responseBody = json_decode($responseData->getBody()->getContents(), true);
-    return [
-        "status" => "OK",
-        "data" => $responseBody,
-    ];
-});
+Route::get('/data', 'External\ResasApiController@request');
