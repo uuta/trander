77b08ced792ba0ad9d1b421cb4de9df9e2f9a084
metal-chaos <nightrage.metal66@gmail.com>
2020-01-25 19:19:15 +0900

    added vue slider library

--- src/resources/js/pages/Setting.vue
+++ src/resources/js/pages/Setting.vue
@@ -2,8 +2,9 @@
   <div id="map_overlay" v-if="settingModal" @click.self="hiddenSettingModal">
     <div id="map_overlay_wrap">
       <div class="container--small">
-        <p>設定（Setting）</p>
+        <h1>設定（Setting）</h1>
         <p>次の地点までの距離</p>
+        <vue-slider ref="slider" v-model="value" :enable-cross="false"></vue-slider>
         <p>3km - 60km</p>
       </div>
     </div>
@@ -13,7 +14,18 @@
 <script>
   import { mapState } from 'vuex'
 
+  import vueSlider from 'vue-slider-component'
+  import 'vue-slider-component/theme/default.css'
+
   export default {
+    components: {
+      vueSlider
+    },
+    data () {
+      return {
+        value: [0, 100],
+      }
+    },
     computed: mapState({
       settingModal: state => state.external.settingModal,
     }),
