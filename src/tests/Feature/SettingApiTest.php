bad63273532842f0054bc0b5462944a0d35abe81
metal-chaos <nightrage.metal66@gmail.com>
2020-01-29 22:47:54 +0900

    added tests for get setting api request

--- src/tests/Feature/SettingApiTest.php
+++ src/tests/Feature/SettingApiTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Setting;
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -22,7 +23,7 @@ class SettingApiTest extends TestCase
     /**
      * @test
      */
-    public function should_setting_APIへのリクエストに成功する()
+    public function should_setting_getにリクエストして正しいレスポンスが返る()
     {
         // テストユーザ作成
         $this->setting = factory(Setting::class)->states('register user and safe distance')->create();
@@ -37,7 +38,7 @@ class SettingApiTest extends TestCase
         ]);
         $response_test->assertStatus(200);
 
-        // setting_APIにリクエストして成功する
+        // setting_get_APIにリクエストして成功する
         $response = $this->get(route('setting.get'));
         $response
             ->assertStatus(200)
@@ -46,5 +47,70 @@ class SettingApiTest extends TestCase
                 'min_distance' => $this->setting->min_distance,
                 'max_distance' => $this->setting->max_distance
             ]);
+    }
+
+    /**
+     * @test
+     */
+    public function should_setting_storeにリクエストしてデータが保存される（作成）()
+    {
+        // テストユーザ作成
+        $this->user = factory(User::class)->create();
+
+        // ログイン
+        $response_test = $this->json('POST', route('login'), [
+            'email' => $this->user->email,
+            'password' => 'secret',
+        ]);
+        $response_test->assertStatus(200);
+
+        $distance = [
+            'lat' => 15,
+            'lng' => 33
+        ];
+        // setting_post_APIにリクエストして成功する
+        $response = $this->post(route('setting.get'), $distance);
+        $response->assertStatus(200);
+
+        // データが保存されていることを確認する
+        $this->assertDatabaseHas('settings', [
+            'user_id' => $this->user->id,
+            'min_distance' => 15,
+            'max_distance' => 33
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function should_setting_storeにリクエストしてデータが保存される（更新）()
+    {
+        // テストユーザ作成
+        $this->setting = factory(Setting::class)->states('register user and safe distance')->create();
+
+        // 作成したテストユーザ検索
+        $user = DB::table('users')->where('id', $this->setting->user_id)->first();
+
+        // ログイン
+        $response_test = $this->json('POST', route('login'), [
+            'email' => $user->email,
+            'password' => 'secret',
+        ]);
+        $response_test->assertStatus(200);
+
+        $distance = [
+            'lat' => $this->setting->min_distance,
+            'lng' => $this->setting->max_distance
+        ];
+        // setting_post_APIにリクエストして成功する
+        $response = $this->post(route('setting.get'), $distance);
+        $response->assertStatus(200);
+
+        // データが保存されていることを確認する
+        $this->assertDatabaseHas('settings', [
+            'user_id' => $this->setting->user_id,
+            'min_distance' => $this->setting->min_distance,
+            'max_distance' => $this->setting->max_distance
+        ]);
     }
 }
