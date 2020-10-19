3789ee0daa00f1c72f09c234096371f48bbff1b5
Yuta Aoki <nightrage.metal66@gmail.com>
2020-10-19 23:18:22 +0900

    Added a tab to switch the way of searching

---
+++ src/resources/js/components/molecules/tab/SearchList.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav class="map_tab">
+    <ul class="lists">
+      <li class="list active">街検索</li>
+      <li class="list">キーワード検索</li>
+    </ul>
+  </nav>
+</template>
--- src/resources/js/components/organisms/index/mapInfo.vue
+++ src/resources/js/components/organisms/index/mapInfo.vue
@@ -1,47 +1,9 @@
 <template>
   <div id="map_info">
+    <SearchList></SearchList>
     <div class="map_info_desc">
-      <dl v-if="cityName" class="map_info_items">
-        <dt class="title"><i class="fas fa-smile"></i>荷物をまとめて出かけよう！</dt>
-        <dd class="list">
-          <img :src="setCountryImg" class="country_flag">
-          <span class="desc">{{region + " " + cityName }}</span>
-        </dd>
-        <dd class="list info">距離：{{distance}} km</dd>
-        <dd class="list info">方角：{{direction}}</dd>
-        <dd class="list">
-          <ul class="flex items">
-            <li class="item"><i class="fas fa-walking" :class="[
-              walking === RECOMMEND_FREQUENCY.NONE ? 'none'
-              : walking === RECOMMEND_FREQUENCY.MIDDLE ? 'middle'
-              : 'high'
-            ]"></i></li>
-            <li class="item"><i class="fas fa-biking" :class="[
-              bycicle === RECOMMEND_FREQUENCY.NONE ? 'none'
-              : bycicle === RECOMMEND_FREQUENCY.MIDDLE ? 'middle'
-              : 'high'
-            ]"></i></li>
-            <li class="item"><i class="fas fa-car" :class="[
-              car === RECOMMEND_FREQUENCY.NONE ? 'none'
-              : car === RECOMMEND_FREQUENCY.MIDDLE ? 'middle'
-              : 'high'
-            ]"></i></li>
-          </ul>
-        </dd>
-        <RouterLink v-if="showAngleBtn" class="negate_btn show_detail" :to="{name: 'cityDetail', params: {cityId: cityId}}">
-          <button class="item_btn"><i class="fas fa-angle-up"></i></button>
-        </RouterLink>
-        <RouterLink v-else class="negate_btn show_detail" :to="{name: 'index'}">
-          <button class="item_btn"><i class="fas fa-angle-right"></i></button>
-        </RouterLink>
-      </dl>
-      <dl class="map_info_introduction" v-else>
-        <dt class="title"><i class="fas fa-street-view"></i>新しい街に行ってみよう</dt>
-        <dd class="list">
-          {{ username }}さん、こんにちは！<br>
-          ボタンを押して、近くの街を探してみましょう。
-        </dd>
-      </dl>
+      <MapInfoItem v-if="cityName"></MapInfoItem>
+      <MapInfoIntroduction v-else></MapInfoIntroduction>
       <transition name="fade">
         <SuggestPushing v-show="suggestPushing"></SuggestPushing>
       </transition>
@@ -53,46 +15,29 @@
 <script>
 import { mapState, mapGetters } from 'vuex'
 import SuggestPushing from '../../../pages/index/Modal/Suggest/Pushing.vue'
-import CONST_EXTERNAL from '../../../const/external.js'
+import MapInfoItem from '../../molecules/mapInfo/Item.vue'
+import MapInfoIntroduction from '../../molecules/mapInfo/Introduction.vue'
+import SearchList from '../../molecules/tab/SearchList.vue'
 
 export default {
   components: {
     SuggestPushing,
-  },
-  created() {
-    this.RECOMMEND_FREQUENCY = CONST_EXTERNAL.RECOMMEND_FREQUENCY
+    MapInfoItem,
+    MapInfoIntroduction,
+    SearchList,
   },
   computed: {
     ...mapState({
       lat: state => state.external.lat,
       lng: state => state.external.lng,
-      countryCode: state => state.external.countryCode,
       currentLat: state => state.external.currentLat,
       currentLng: state => state.external.currentLng,
       rangeOfDistance: state => state.external.rangeOfDistance,
       cityName: state => state.external.cityName,
-      region: state => state.external.region,
       suggestPushing: state => state.external.suggestPushing,
-      distance: state => state.external.distance,
-      direction: state => state.external.direction,
       directionType: state => state.external.directionType,
-      walking: state => state.external.walking,
-      bycicle: state => state.external.bycicle,
-      car: state => state.external.car,
       wikiDataId: state => state.external.wikiDataId,
-      cityId: state => state.external.cityId,
     }),
-    ...mapGetters({
-      username: 'auth/username'
-    }),
-    setCountryImg: function() {
-      if (this.countryCode != null) {
-        return 'https://www.countryflags.io/' + this.countryCode + '/flat/32.png'
-      }
-    },
-    showAngleBtn: function() {
-      return !Object.keys(this.$route.params).length
-    },
   },
   methods: {
     setNewLocation() {
