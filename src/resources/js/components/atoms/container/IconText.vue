6e02776f5fdbcab088c07651bf492315b29ac435
Yuta Aoki <nightrage.metal66@gmail.com>
2020-11-01 21:04:50 +0900

    Added a modal for sharing to twitter

--- src/resources/js/components/atoms/container/IconText.vue
+++ src/resources/js/components/atoms/container/IconText.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="c-wrap_icon_text">
+  <div :class="className">
     <img :src="image" class="icon" :alt="text"><span class="description">{{text}}</span>
   </div>
 </template>
@@ -10,6 +10,7 @@ export default {
   props: {
     text: { type: String, required: true },
     image: { type: String, required: true },
+    className: { type: String, required: true },
   },
 }
 </script>
--- src/resources/js/components/organisms/kw/Buttons.vue
+++ src/resources/js/components/organisms/kw/Buttons.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="p-map_kw_google_btns">
-    <div class="container">
+    <div class="container wide">
+      <IconText
+        text="ツイートしてシェアする！"
+        :image="twitter"
+        className="c-wrap_icon_text"
+      ></IconText>
+    </div>
+    <div class="container general">
       <IconText
         text="Google Map"
         :image="googleMap"
+        className="c-wrap_icon_text"
       ></IconText>
     </div>
-    <div class="container">
+    <div class="container general">
       <IconText
         text="Street View"
         :image="streetView"
+        className="c-wrap_icon_text"
       ></IconText>
     </div>
   </div>
@@ -21,6 +30,7 @@ import IconText from '../../atoms/container/IconText.vue'
 export default {
   data() {
     return {
+      twitter: '/assets/icons/kws/twitter_2.svg',
       googleMap: '/assets/icons/kws/google-maps.svg',
       streetView: '/assets/icons/kws/street-view.svg'
     }
