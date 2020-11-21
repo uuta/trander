ea96a7162f594edce7e82361d0c34c765b77815b
Yuta Aoki <nightrage.metal66@gmail.com>
2020-11-21 19:19:30 +0900

    Added google_place_ids table

---
+++ src/database/migrations/2020_11_16_212229_create_google_place_ids.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGooglePlaceIds extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('google_place_ids', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('place_id')->index()->comment('Place ID');
+            $table->string('name')->comment('Place name');
+            $table->float('rating')->nullable()->comment('Average of rating');
+            $table->string('photo')->nullable()->comment('Photo ID');
+            $table->string('vicinity')->nullable()->comment('Address');
+            $table->integer('user_ratings_total')->nullable()->comment('Sum of ratings');
+            $table->integer('price_level')->nullable()->comment('Price Level');
+            $table->float('lat')->comment('Latitude');
+            $table->float('lng')->comment('Longitude');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+            $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('google_place_ids');
+    }
+}
