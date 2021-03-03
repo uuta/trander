da9f4f8d296751cc8f51ebd708ba8e6a1354f7fd
Yuta Aoki <nightrage.metal66@gmail.com>
2021-03-03 22:13:17 +0900

    Added a configuration with regard to CSRF for accepting a request from external API

--- src/config/const.php
+++ src/config/const.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
-  'title_description' => env('TITLE_DESCRIPTION'),
-  'meta_description' => env('META_DESCRIPTION'),
-  'og_type' => env('OG_TYPE'),
+    'title_description' => env('TITLE_DESCRIPTION'),
+    'meta_description' => env('META_DESCRIPTION'),
+    'og_type' => env('OG_TYPE'),
+
+    'security' => [
+        'access_control_allow_origin' => env('ACCESS_CONTROL_ALLOW_ORIGIN'),
+        'csrf_excerpt' => env('CSRF_EXCEPT'),
+    ]
 ];
