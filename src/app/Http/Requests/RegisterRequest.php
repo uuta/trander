d7bdf54f7d2673bf06f5a8bb38a9bdb837135178
Yuta Aoki <nightrage.metal66@gmail.com>
2021-07-10 18:53:50 +0900

    Customized validation to English

--- src/app/Http/Requests/RegisterRequest.php
+++ src/app/Http/Requests/RegisterRequest.php
@@ -29,8 +29,6 @@ class RegisterRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'terms_of_service' => ['accepted'],
-            'privacy_policy' => ['accepted'],
         ];
     }
 
