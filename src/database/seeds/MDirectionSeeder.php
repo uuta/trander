2f93e428edad724002a37b2d75c146ea6329b8f9
Yuta Aoki <nightrage.metal66@gmail.com>
2022-01-14 14:06:54 +0900

    Changed a seeder file to insert direction names in English

--- src/database/seeds/MDirectionSeeder.php
+++ src/database/seeds/MDirectionSeeder.php
@@ -13,15 +13,15 @@ class MDirectionSeeder extends Seeder
     public function run()
     {
         $seeder = [
-            ['direction_id' => 1, 'direction_name' => '北', 'min_angle' => 337.5, 'max_angle' => 360, 'created_at' => now()],
-            ['direction_id' => 2, 'direction_name' => '北', 'min_angle' => 0, 'max_angle' => 22.5, 'created_at' => now()],
-            ['direction_id' => 3, 'direction_name' => '北東', 'min_angle' => 22.5, 'max_angle' => 67.5, 'created_at' => now()],
-            ['direction_id' => 4, 'direction_name' => '東', 'min_angle' => 67.5, 'max_angle' => 112.5, 'created_at' => now()],
-            ['direction_id' => 5, 'direction_name' => '南東', 'min_angle' => 112.5, 'max_angle' => 157.5, 'created_at' => now()],
-            ['direction_id' => 6, 'direction_name' => '南', 'min_angle' => 157.5, 'max_angle' => 202.5, 'created_at' => now()],
-            ['direction_id' => 7, 'direction_name' => '南西', 'min_angle' => 202.5, 'max_angle' => 247.5, 'created_at' => now()],
-            ['direction_id' => 8, 'direction_name' => '西', 'min_angle' => 247.5, 'max_angle' => 292.5, 'created_at' => now()],
-            ['direction_id' => 9, 'direction_name' => '北西', 'min_angle' => 292.5, 'max_angle' => 337.5, 'created_at' => now()],
+            ['direction_id' => 1, 'direction_name' => 'North', 'min_angle' => 337.5, 'max_angle' => 360, 'created_at' => now()],
+            ['direction_id' => 2, 'direction_name' => 'North', 'min_angle' => 0, 'max_angle' => 22.5, 'created_at' => now()],
+            ['direction_id' => 3, 'direction_name' => 'North East', 'min_angle' => 22.5, 'max_angle' => 67.5, 'created_at' => now()],
+            ['direction_id' => 4, 'direction_name' => 'East', 'min_angle' => 67.5, 'max_angle' => 112.5, 'created_at' => now()],
+            ['direction_id' => 5, 'direction_name' => 'South East', 'min_angle' => 112.5, 'max_angle' => 157.5, 'created_at' => now()],
+            ['direction_id' => 6, 'direction_name' => 'South', 'min_angle' => 157.5, 'max_angle' => 202.5, 'created_at' => now()],
+            ['direction_id' => 7, 'direction_name' => 'South West', 'min_angle' => 202.5, 'max_angle' => 247.5, 'created_at' => now()],
+            ['direction_id' => 8, 'direction_name' => 'West', 'min_angle' => 247.5, 'max_angle' => 292.5, 'created_at' => now()],
+            ['direction_id' => 9, 'direction_name' => 'North West', 'min_angle' => 292.5, 'max_angle' => 337.5, 'created_at' => now()],
         ];
         DB::table('m_directions')->insert($seeder);
     }
