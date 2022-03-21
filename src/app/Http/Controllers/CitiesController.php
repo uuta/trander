3f807b7d8d1a7929b8a34af3bdb9bd1145dc27cd
Yuta Aoki <nightrage.metal66@gmail.com>
2022-03-21 15:01:14 +0900

    #57: Deleted unnecessary class

--- src/app/Http/Controllers/CitiesController.php
+++ src/app/Http/Controllers/CitiesController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\Cities\IndexService;
 use App\Http\Models\RequestCountHistory;
-use App\Http\Resources\Cities\IndexResource;
 use App\UseCases\Cities\Index\CityIndexUseCase;
 use App\Http\Requests\Cities\CitiesIndexRequest;
 use App\Services\Facades\GenerateLocationService;
