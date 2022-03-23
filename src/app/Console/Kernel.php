d649b2627203b61647bcebd7a2c209731af524be
Yuta Aoki <nightrage.metal66@gmail.com>
2022-03-23 17:29:25 +0900

    #57: Registerd a command to scheduler

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Console\Commands\RestoreRequestLimitCommand;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -24,8 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command(RestoreRequestLimitCommand::class)->everyTenMinutes()->runInBackground();
     }
 
     /**
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
